72cc5e72b10ac5def1ddedf328675026dac6e574
Brant Han <brant.han@gmail.com>
2019-08-13 22:32:32 -0700

    add hyperlink to id on page plus css

--- resources/javascript/app.js
+++ resources/javascript/app.js
@@ -4,5 +4,13 @@ $(document).ready(function() {
         $("#tada").replaceWith("<h2>TA DA!</h2>");
         $("#gone").replaceWith("<p>Look...its gone.</p>");
         $("button").hide();
+
+        var link = $("<a>");
+        link.attr("href", "https://monsterreh2b.github.io/eight_year_old/");
+        link.attr("title", "Magic Trick");
+        link.text("Start Over");
+        link.addClass("link");
+
+        $("#start-over").html(link);
     });
 });
