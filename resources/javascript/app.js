4287cee5dae2c28c7664f02441e8b168049c0bf6
Brant Han <brant.han@gmail.com>
2019-08-15 22:40:53 -0700

    added gif to endpage

--- resources/javascript/app.js
+++ resources/javascript/app.js
@@ -5,6 +5,17 @@ $(document).ready(function() {
         $("#gone").replaceWith("<p>Look...its gone.</p>");
         $("button").hide();
 
+        var clap = $("<iframe>");
+        clap.attr("src", "https://giphy.com/embed/9o9MxN7wtaIK4GawF9");
+        clap.attr("width", "480");
+        clap.attr("height", "360");
+        clap.attr("class", "giphy-embed");
+
+        $("#next-image").html(clap);
+
+
+
+
         var link = $("<a>");
         link.attr("href", "https://monsterreh2b.github.io/eight_year_old/");
         link.attr("title", "Magic Trick");
